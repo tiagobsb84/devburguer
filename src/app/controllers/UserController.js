34b50ebc07bdadbc27c069b83f105480c9ac92cb
Tiago Silva <tiagobsb84@gmail.com>
2024-07-16 06:42:39 -0300

    Instalei yup para validacoes e esta tratado erros de validacoes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,23 @@
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 
 class UserController {
     async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch(err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
         const { name, email, password_hash, admin } = request.body;
 
         const user = await User.create({
