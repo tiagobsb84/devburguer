81c20372804216829164fd9d35344ba4a5048a77
Tiago Silva <tiagobsb84@gmail.com>
2024-07-16 07:03:09 -0300

    evitando email duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,6 +20,16 @@ class UserController {
 
         const { name, email, password_hash, admin } = request.body;
 
+        const userExists = await User.findOne({
+            where: {
+                email
+            }
+        });
+
+        if(userExists) {
+            return response.status(400).json({error: 'User already exists'});
+        }
+
         const user = await User.create({
             id: v4(),
             name,
