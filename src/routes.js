ecfa00503f891ce6f22aafa5ab1c3f8152e267a8
Tiago Silva <tiagobsb84@gmail.com>
2024-11-08 18:32:09 -0300

    upload das imagens com o multer

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductsController from './app/controllers/ProductsController';
+import multerConfig from './config/multer';
+
+import multer from 'multer';
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
-routes.post('/products', ProductsController.store);
+routes.post('/products', upload.single('file'), ProductsController.store);
 
 export default routes;
