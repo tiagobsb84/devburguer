3a945b9389286fcd1bb0549399699f6fb8740b69
Tiago Silva <tiagobsb84@gmail.com>
2024-11-08 17:37:52 -0300

    criando o controler de produtos

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProductsController from './app/controllers/ProductsController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.post('/products', ProductsController.store);
 
 export default routes;
