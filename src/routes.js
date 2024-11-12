cda3f86ae5eeb778006ca87c0d6a2998157d8782
Tiago Silva <tiagobsb84@gmail.com>
2024-11-12 15:49:21 -0300

    autenticando as rotas para passaram token

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductsController from './app/controllers/ProductsController';
 import multerConfig from './config/multer';
+import authMiddleware from './middlewares/auth';
 
 import multer from 'multer';
 
@@ -12,7 +13,10 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+//Com isso todas as rotas abaixo do metodo use() receberão autenticação.
+routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductsController.store);
-routes.get('/products', ProductsController.index);
+routes.get('/products', authMiddleware, ProductsController.index);
 
 export default routes;
